2823b7b03ec22043eed896f80cf98554f8b50594
Sylvain MARIE <sylvain.marie@se.com>
2019-08-09 18:01:44 +0200

    Travis now should run both meta tests and test cases

--- ci_tools/run_tests.sh
+++ ci_tools/run_tests.sh
@@ -13,14 +13,19 @@ cleanup() {
 trap "cleanup" INT TERM EXIT
 
 if [ "${TRAVIS_PYTHON_VERSION}" = "3.5" ]; then
-   # full
-   # Run tests with "python -m pytest" to use the correct version of pytest
-   echo -e "\n\n****** Running tests ******\n\n"
-   coverage run --source pytest_pilot -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html -v pytest_pilot/tests/
-   # buggy
-   # python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./pytest_pilot -v pytest_pilot/tests/
+    # full
+    # First the raw for coverage
+    echo -e "\n\n****** Running tests : 1/2 RAW******\n\n"
+    coverage run --source pytest_pilot -m pytest -v pytest_pilot/test_cases/
+    # python -m pytest --cov-report term-missing --cov=./pytest_pilot -v pytest_pilot/test_cases/
+
+    # Then the meta (appended)
+    echo -e "\n\n****** Running tests : 2/2 META******\n\n"
+    coverage run --append --source pytest_pilot -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html -v pytest_pilot/tests/
+    # buggy
+    # python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./pytest_pilot --cov-append -v pytest_pilot/tests/
 else
-   # faster - skip coverage and html report but keep junit (because used in validity threshold)
+    # faster - skip coverage and html report but keep junit (because used in validity threshold)
     echo -e "\n\n****** Running tests******\n\n"
     python -m pytest --junitxml=reports/junit/junit.xml -v pytest_pilot/tests/
 fi
