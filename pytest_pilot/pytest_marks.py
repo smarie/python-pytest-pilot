717449217b526929d8f12f1aa4b0cfe59ecef53f
Sylvain MARIE <sylvain.marie@se.com>
2020-09-29 11:42:11 +0200

    EasyMarker's mode argument is now mandatory

--- pytest_pilot/pytest_marks.py
+++ pytest_pilot/pytest_marks.py
@@ -42,12 +42,12 @@ class EasyMarker(object):
 
     def __init__(self,
                  marker_id,             # type: str
+                 mode,                  # type: str
                  full_name=None,        # type: str
                  has_arg=True,          # type: bool
                  allowed_values=None,   # type: Iterable[Any]
                  cmdoption_short=None,  # type: str
                  cmdoption_long=None,   # type: str
-                 mode='extender',       # type: str
                  cmdhelp=None,          # type: str
                  markhelp=None,         # type: str
                  ):
@@ -58,6 +58,16 @@ class EasyMarker(object):
 
         :param marker_id: the name of the pytest mark. Applying this marker with `@marker(arg)` will be equivalent to
             applying @pytest.mark.<marker_id>(arg)
+        :param mode: a mandatory string indicating the working mode of this mark and the associated filter option. Four
+            modes are supported:
+             - 'silos': When the option is inactive, only non-marked tests are run. When the option is active, only
+               relevant marked tests run. There is no test in common between these "silos"
+             - 'extender': When the option is inactive, only non-marked tests are run, this is the "base" set
+               of tests. When the option is active, it adds the relevant marked tests to the base set.
+             - 'hard_filter': When the option is inactive, all tests run. When the option is active, only the relevant
+               marked tests run.
+             - 'soft_filter': When the option is inactive, all tests run. When the option is active, all non-marked
+               tests continue to run, but among marked tests only the relevant ones run.
         :param full_name: the full name of the marker, to be used in help texts. If `None` (default), it defaults to
             `marker_id`.
         :param has_arg: if this is `True` (default), the marker has a single argument and the filtering commandline
@@ -73,16 +83,6 @@ class EasyMarker(object):
             name collisions.
         :param cmdoption_long: the id to use for the "long" command option (for example providing `'env'` or `'--env'`
             will result in the option `'--env'`). `None` (default) will use `marker_id` for the long command option.
-        :param mode: a string indicating the working mode of this mark and the associated filter option. Four modes
-            are supported:
-             - 'silos': When the option is inactive, only non-marked tests are run. When the option is active, only
-               relevant marked tests run. There is no test in common between these "silos"
-             - 'extender' (default): When the option is inactive, only non-marked tests are run, this is the "base" set
-               of tests. When the option is active, it adds the relevant marked tests to the base set.
-             - 'hard_filter': When the option is inactive, all tests run. When the option is active, only the relevant
-               marked tests run.
-             - 'soft_filter': When the option is inactive, all tests run. When the option is active, all non-marked
-               tests continue to run, but among marked tests only the relevant ones run.
         :param cmdhelp: the help message displayed when `pytest --help` is called
         :param markhelp: the help message displayed when `pytest --markers` is called
         """
